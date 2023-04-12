7675175e01f6d4af90e7d6cc99904cab411003d2
TheDusty117 <gabrieledipassio@gmail.com>
2023-04-12 16:12:13 +0200

    FEAT creating columns for 'trains' table

--- database/migrations/2023_04_12_135109_create_trains_table.php
+++ database/migrations/2023_04_12_135109_create_trains_table.php
@@ -14,7 +14,18 @@ return new class extends Migration
     public function up()
     { //tabella trains creata
         Schema::create('trains', function (Blueprint $table) {
-            $table->id();
+            $table->id(); //qui sotto aggiungo specifiche tabella
+            $table->string('azienda',100);
+            $table->string('stazione_partenza',50);
+            $table->string('stazione_arrivo',50);
+            $table->time('orario_partenza', $precision = 0);
+            $table->time('orario_arrivo', $precision = 0);
+            $table->string('codice_treno',5);
+            $table->string('numero_carrozze',2);
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+
+
             $table->timestamps();
         });
     }
